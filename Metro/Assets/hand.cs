e4eec487e0bf846760ebc60e3f795ade229d0b0e
Mari Kyle <Marimkyle@gmail.com>
2016-11-30 10:27:53 -0800

    third paid aggressor train

---
+++ Metro/Assets/hand.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hand : MonoBehaviour {
+	public GameObject handag;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+
+	void OnTriggerEnter() {
+		print ("hey!");
+		handag.SetActive(true);
+
+	}
+
+}
---
+++ Metro/Assets/remybye.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class remybye : MonoBehaviour {
+	public GameObject rem;
+	public GameObject audiyou;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+
+	void OnTriggerEnter() {
+		print ("hey!");
+		rem.SetActive(false);
+		audiyou.SetActive (true);
+
+	}
+
+}
